fbed3a5986f8d6e10b7ca9fffb200e9e86ae42b7
charlie <charliealonso2001@gmail.com>
2021-09-20 12:49:31 -0500

    setting up redux

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
     this.state = {
       currentUser: null
-    }
+    };
   }
 
   componentWillUnmount() {
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -20,12 +20,12 @@ const Header = ({ currentUser }) => (
                 CONTACT
             </Link>
             {
-                currentUser ?
-                <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>
+                currentUser ? 
+                (<div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>)
                 :
-                <Link className='option' to='/signin'>
+                (<Link className='option' to='/signin'>
                     SIGN IN
-                </Link>
+                </Link>)
             }
         </div>
     </div>
